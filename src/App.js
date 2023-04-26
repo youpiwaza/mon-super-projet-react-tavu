3325f3421ac8b0f20d92de5eaf04bd34eb9ed542
max <masamune.code@gmail.com>
2023-04-26 16:49:37 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+      <MyButton />
+      <MyButton />
+      <MyButton />
+    </div>
+  );
+}
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return <button onClick={handleClick}>Clicked {count} times</button>;
+}
